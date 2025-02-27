8a95100b98bdf79ae4a0604a864bbf004aee8786
CJ-2025 <202280263@psu.palawan.edu.ph>
2025-02-27 10:39:44 +0800

    checker for connection

--- fake_data.php
+++ fake_data.php
@@ -3,4 +3,9 @@ require 'conn.php';
 require 'vendor/autoload.php';
 
 $faker = Faker\Factory::create('en_PH');
+
+if (!$mysqli) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
 ?>
