1cd665a314138fb9801d9b71609f8ca188cc4fac
CJ-2025 <202280263@psu.palawan.edu.ph>
2025-02-27 10:40:28 +0800

    Disabling Foreign Key Checks

--- fake_data.php
+++ fake_data.php
@@ -8,4 +8,10 @@ if (!$mysqli) {
     die("Database connection failed: " . mysqli_connect_error());
 }
 
+$mysqli->query("SET FOREIGN_KEY_CHECKS = 0");
+$mysqli->query("TRUNCATE TABLE office");
+$mysqli->query("TRUNCATE TABLE employee");
+$mysqli->query("TRUNCATE TABLE transaction");
+$mysqli->query("SET FOREIGN_KEY_CHECKS = 1");
+
 ?>
